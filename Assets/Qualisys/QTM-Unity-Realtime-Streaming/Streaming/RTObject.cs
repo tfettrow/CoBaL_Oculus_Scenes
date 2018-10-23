68622c8c2bab8fbf000bcf4825558bd6468e3d22
Linden003 <42500130+Linden003@users.noreply.github.com>
2018-10-23 14:21:22 -0400

    Initial commit

---
+++ Assets/Qualisys/QTM-Unity-Realtime-Streaming/Streaming/RTObject.cs
@@ -0,0 +1,41 @@
+﻿// Unity SDK for Qualisys Track Manager. Copyright 2015-2017 Qualisys AB
+//
+using UnityEngine;
+using System.Collections;
+using QTMRealTimeSDK;
+
+namespace QualisysRealTime.Unity
+{
+    class RTObject : MonoBehaviour
+    {
+        public string ObjectName = "Put QTM 6DOF object name here";
+        public Vector3 PositionOffset = new Vector3(0, 0, 0);
+        public Vector3 EulerOffset = new Vector3(0, 0, 0);
+
+        private RTClient rtClient;
+
+        // Use this for initialization
+        void Start()
+        {
+            rtClient = RTClient.GetInstance();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (rtClient == null) rtClient = RTClient.GetInstance();
+
+            SixDOFBody body = rtClient.GetBody(ObjectName);
+            if (body != null)
+            {
+                if (body.Position.magnitude > 0) //just to avoid error when position is NaN
+                {
+                    transform.position = body.Position + PositionOffset;
+                    if (transform.parent) transform.position += transform.parent.position;
+                    transform.rotation = body.Rotation * Quaternion.Euler(EulerOffset);
+                    if (transform.parent) transform.rotation *= transform.parent.rotation;
+                }
+            }
+        }
+    }
+}
